71ff2a96d5640ef82f48910b529da77bc897c9de
Eduardo Cueto Mendoza <cueto303@gmail.com>
2020-07-31 10:34:37 -0600

    Finished the processing functions after getting cpu benchmarks

--- src/neflops/hwflops.jl
+++ src/neflops/hwflops.jl
@@ -10,19 +10,45 @@ function getcpuclock()
 end
 
 
-function getcpuusage()
-    tempfile = "./src/neflops/cpuper"
-    cpuusage = pipeline(`top`, `grep \%CPU`, tempfile)
-    ch = Channel(1)
-    put!(ch,@async run(cpuusage))
-    sleep(60)
-    if isready(ch)
-        take!(ch)
+function getcpudata(nosamples = 60)
+    script = "#!/bin/bash\n\nx=1\nwhile [ \$x -le $nosamples ]\ndo\n  timeout 1s top -b > cpuusg\n  sleep 2\n  grep \"%Cpu\" cpuusg >> cpudata\n  x=\$(( \$x + 1 ))\ndone\n"
+    open("./tempscript.sh", "w") do f
+        write(f, script)
+    end 
+    runscript = `bash tempscript.sh`
+    run(runscript)
+    sleep(1)
+    rm("./tempscript.sh")
+    rm("./cpuusg")
+end
+
+function processcpudata(nosamples = 60)
+    data = open("./cpudata") do f
+        read(f,String)
     end
-    data = open(tempfile) do text
-        read(text, String)
-    end
-    rm(tempfile)
+    rm("./cpudata")
     data = split(data, "\n")
-    return data
+    cpudata = Array{String}[]
+    for m in data
+        push!(cpudata, split(m," "))
+    end
+    cpuusg = Float64[]
+    for d in cpudata[1:nosamples]
+        push!(cpuusg, parse(Float64,d[3]) + parse(Float64,d[6]))
+    end
+    return cpuusg
+end
+
+
+"""
+Returns a estimation of the GFLOPs that were performed during the functions runtime
+"""
+function estcpuflops(nosamples = 60)
+    meanflops = open("./src/neflops/cpuflops") do f
+        read(f, String)
+    end
+    getcpudata(nosamples)
+    meanflops = parse(Float64,meanflops)
+    cpuusg = processcpudata(nosamples)
+    return meanflops * (mean(cpuusg)/100.0)
 end
